4f797dca4a0f3118ad9a122507275048fb5edbaa
alfisha-28 <ansarialfisha2006@gmail.com>
2025-07-08 23:42:26 +0530

    Components Type

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,17 @@ import Form from './Form.jsx';
 import Counter from './Counter.jsx';
 import LikeButton from './LikeButton.jsx';
 import ToDoList from './ToDoList.jsx';
+import Lottery from './Lottery.jsx';
+import Ticket from './Ticket.jsx';
 
 function App() {
   return (
     <>
       <div>
-        <ToDoList/>
+        <Ticket ticket={[0, 1, 2]}/>
+        <Ticket ticket={[0, 1, 2, 3, 4, 5, 6]}/>
+        {/* <Lottery/> */}
+        {/* <ToDoList/> */}
         {/* <LudoBoard/> */}
         {/* <ProductTab/> */}
         {/* <Msgbox userName="Alfisha" textColor="blue"/> */}
@@ -23,7 +28,7 @@ function App() {
       </div>
 
     </>
-  )
+  );
 }
 
 export default App;
